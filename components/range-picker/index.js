2e450a890afaa8577c3ee7407eadede7fcabbdfb
superpuper32 <pochitalin_m@mail.ru>
2020-02-15 00:51:13 +0300

    rangepicker time from fixed

--- components/range-picker/index.js
+++ components/range-picker/index.js
@@ -3,6 +3,7 @@ import createElement from "../../lib/create-element.js";
 export default class Rangepicker {
   constructor() {
     this.time = new Date();
+    this.from = new Date();
     this.render();
   }
 
@@ -10,7 +11,8 @@ export default class Rangepicker {
     this.elem = createElement(
       `div class="rangepicker">
             <div class="rangepicker__input">
-                <span>1/13/20 </span>
+                <span>${this.from.getMonth() + 1}/${this.from.getDate() -
+        14}/${this.from.getFullYear()} </span>
                 - <span> ${this.time.getMonth() +
                   1}/${this.time.getDate()}/${this.time.getFullYear()}</span>
             </div>
