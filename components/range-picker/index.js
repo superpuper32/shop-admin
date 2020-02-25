adf9c28dd8265c2d69cc15abbbe543964fbdd16a
superpuper32 <pochitalin_m@mail.ru>
2020-02-25 21:11:01 +0300

    render calendar first day starts after a space * * * 1 2 3

--- components/range-picker/index.js
+++ components/range-picker/index.js
@@ -1,24 +1,30 @@
 import createElement from "../../lib/create-element.js";
 
 export default class Rangepicker {
-  constructor() {
-    this.time = new Date();
-    this.from = new Date();
+  constructor({ from, to }) {
+    this.showDateFrom = new Date(from);
+    this.selected = { from, to };
     this.render();
+  }
+
+  getDay(date) {
+    let day = date.getDay();
+    return day === 0 ? 6 : day - 1;
   }
 
   render() {
     this.elem = createElement(
-      `div class="rangepicker">
-            <div class="rangepicker__input">
-                <span data-elem="from">${this.from.getMonth() +
-                  1}/${this.from.getDate() -
-        14}/${this.from.getFullYear()} </span>
-                - <span data-elem="to"> ${this.time.getMonth() +
-                  1}/${this.time.getDate()}/${this.time.getFullYear()}</span>
-                  <div class="rangepicker__selector" data-elem="selector"></div>
-            </div>
-        </div>`
+      `<div class="rangepicker">
+      <div class="rangepicker__input" data-elem="input">
+      <span data-elem="from">${this.selected.from.toLocaleString("default", {
+        dateStyle: "short"
+      })}</span> - 
+      <span data-elem="to">${this.selected.to.toLocaleString("default", {
+        dateStyle: "short"
+      })}</span>
+      </div>
+      <div class="rangepicker__selector" data-elem="selector"></div>
+      </div>`
     );
 
     this.elem.onclick = () => this.toggle();
@@ -30,20 +36,25 @@ export default class Rangepicker {
   }
 
   renderSelector() {
+    let showDate1 = new Date(this.showDateFrom);
     this.elem.querySelector(
       ".rangepicker__selector"
     ).innerHTML = `<div class="rangepicker__selector_arrow"></div>
       <div class="rangepicker__selector-control rangepicker__selector-control_left"></div>
       <div class="rangepicker__selector-control rangepicker__selector-control_right"></div>
-      ${this.renderCalendar()}
-      ${this.renderCalendar()}
+      ${this.renderCalendar(showDate1)}
     `;
   }
 
-  renderCalendar() {
+  renderCalendar(showDate) {
+    let date = new Date(showDate);
+    date.setDate(1);
+
+    let monthStr = date.toLocaleString("default", { month: "long" });
+
     let calendar = `<div class="rangepicker__calendar">
       <div class="rangepicker__month">
-        <time datetime="Февраль">Февраль</time>
+        <time datetime="Февраль">${monthStr}</time>
       </div>
       <div class="rangepicker__day">
         <div>Пн</div><div>Вт</div><div>Ср</div><div>Чт</div><div>Пт</div><div>Сб</div><div>Вс</div>
@@ -51,11 +62,20 @@ export default class Rangepicker {
       <div class="rangepicker__date-grid">
     `;
 
+    calendar += `<button type="button" class="rangepicker__cell" data-value="${date.toISOString()}" style="--start-from: ${this.getDay(
+      date
+    ) + 1}">${date.getDate()}</button>`;
+
     calendar += `</div></div>`;
 
     return calendar;
   }
 }
 
-let rangepicker = new Rangepicker();
+let range = {
+  from: new Date(Date.now() - 30 * 86400e3),
+  to: new Date()
+};
+
+let rangepicker = new Rangepicker(range);
 document.querySelector(".content__top-panel").append(rangepicker.elem);
