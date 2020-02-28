18292a83bb9b84fcbb2f61471acbdbaebd08bc9d
superpuper32 <pochitalin_m@mail.ru>
2020-02-28 18:47:22 +0300

    doubleslider pointer event left thumb added

--- components/double-slider/index.js
+++ components/double-slider/index.js
@@ -7,6 +7,9 @@ export default class DoubleSlider {
     this.formatValue = formatValue;
     this.selected = selected || { from: min, to: max };
 
+    this.onThumbPointerMove = this.onThumbPointerMove.bind(this);
+    this.onThumbPointerUp = this.onThumbPointerUp.bind(this);
+
     this.render();
   }
 
@@ -21,34 +24,83 @@ export default class DoubleSlider {
       <span data-elem="to" class="range-slider__price range-slider__price_to"></span>
     </div>`);
 
+    this.elem.ondragstart = () => false;
+
+    this.elems = {};
+    for (let subElem of this.elem.querySelectorAll("[data-elem]")) {
+      this.elems[subElem.dataset.elem] = subElem;
+    }
+
+    this.elems.thumbLeft.addEventListener("pointerdown", e =>
+      this.onThumbPointerDown(e)
+    );
+
     this.update();
   }
 
   update() {
     let rangeTotal = this.max - this.min;
 
-    this.elem.querySelector(".range-slider__progress").style.left =
+    this.elems.progress.style.left =
       Math.floor(((this.selected.from - this.min) / rangeTotal) * 100) + "%";
-
-    this.elem.querySelector(".range-slider__progress").style.right =
+    this.elems.progress.style.right =
       Math.floor(((this.max - this.selected.to) / rangeTotal) * 100) + "%";
 
-    this.elem.querySelector(
-      ".range-slider__price_from"
-    ).innerHTML = this.formatValue(this.selected.from);
+    this.elems.from.innerHTML = this.formatValue(this.selected.from);
+    this.elems.to.innerHTML = this.formatValue(this.selected.to);
+  }
 
-    this.elem.querySelector(
-      ".range-slider__price_to"
-    ).innerHTML = this.formatValue(this.selected.to);
+  onThumbPointerDown(e) {
+    let thumbElem = e.target;
+    e.preventDefault();
+
+    let thumbCoords = thumbElem.getBoundingClientRect();
+
+    if (thumbElem === this.elems.thumbLeft) {
+      this.shiftX = thumbCoords.right - event.clientX;
+    } else {
+      this.shiftX = thumbCoords.left - event.clientX;
+    }
+
+    this.dragging = thumbElem;
+
+    this.elem.classList.add("range-slider_dragging");
+
+    document.addEventListener("pointermove", this.onThumbPointerMove);
+    document.addEventListener("pointerup", this.onThumbPointerUp);
+  }
+
+  onThumbPointerMove(e) {
+    e.preventDefault();
+
+    if (this.dragging === this.elems.thumbLeft) {
+      let newLeft =
+        (event.clientX -
+          this.elems.inner.getBoundingClientRect().left +
+          this.shiftX) /
+        this.elems.inner.offsetWidth;
+      if (newLeft < 0) newLeft = 0;
+      newLeft *= 100;
+      let right = parseFloat(this.elems.thumbRight.style.right);
+      if (newLeft + right > 100) newLeft = 100 - right;
+      this.dragging.style.left = this.elems.progress.style.left = newLeft + "%";
+    }
+  }
+
+  onThumbPointerUp() {
+    this.elem.classList.remove("range-slider_dragging");
+
+    document.removeEventListener("pointermove", this.onThumbPointerMove);
+    document.removeEventListener("pointerup", this.onThumbPointerUp);
   }
 }
+
+// Dashboard page
 
 let slider = new DoubleSlider({
   min: 0,
   max: 4000,
-  formatValue: value => {
-    return "$" + value;
-  }
+  formatValue: value => "$" + value
 });
 
 document.querySelector(".content__top-panel").append(slider.elem);
