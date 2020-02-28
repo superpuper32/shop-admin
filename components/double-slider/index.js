6d1b4eba000a184101d9d4cfc3c5b7c30e1bc664
superpuper32 <pochitalin_m@mail.ru>
2020-02-28 16:49:08 +0300

    doubleslider update from to price method added

--- components/double-slider/index.js
+++ components/double-slider/index.js
@@ -1,31 +1,54 @@
 import createElement from "../../lib/create-element.js";
 
 export default class DoubleSlider {
-  constructor(min, max, formatValue) {
+  constructor({ min, max, formatValue, selected }) {
     this.min = min;
     this.max = max;
     this.formatValue = formatValue;
+    this.selected = selected || { from: min, to: max };
 
     this.render();
   }
 
   render() {
     this.elem = createElement(`<div class="range-slider">
-      <span data-elem="from" class="range-slider__price"></span>
+      <span data-elem="from" class="range-slider__price range-slider__price_from"></span>
       <div data-elem="inner" class="range-slider__inner">
         <span data-elem="progress" class="range-slider__progress"></span>
-        <span data-elem="thumbLeft" class="range-slider__thumb-left"></span>
-        <span data-elem="thumbRight" class="range-slider__thumb-right"></span>
+        <span data-elem="thumbLeft" class="range-slider__thumb range-slider__thumb_left"></span>
+        <span data-elem="thumbRight" class="range-slider__thumb range-slider__thumb_right"></span>
       </div>
-      <span data-elem="to" class="range-slider__price"></span>
+      <span data-elem="to" class="range-slider__price range-slider__price_to"></span>
     </div>`);
+
+    this.update();
+  }
+
+  update() {
+    let rangeTotal = this.max - this.min;
+
+    this.elem.querySelector(".range-slider__progress").style.left =
+      Math.floor(((this.selected.from - this.min) / rangeTotal) * 100) + "%";
+
+    this.elem.querySelector(".range-slider__progress").style.right =
+      Math.floor(((this.max - this.selected.to) / rangeTotal) * 100) + "%";
+
+    this.elem.querySelector(
+      ".range-slider__price_from"
+    ).innerHTML = this.formatValue(this.selected.from);
+
+    this.elem.querySelector(
+      ".range-slider__price_to"
+    ).innerHTML = this.formatValue(this.selected.to);
   }
 }
 
 let slider = new DoubleSlider({
   min: 0,
   max: 4000,
-  formatValue: value => "$" + value
+  formatValue: value => {
+    return "$" + value;
+  }
 });
 
 document.querySelector(".content__top-panel").append(slider.elem);
